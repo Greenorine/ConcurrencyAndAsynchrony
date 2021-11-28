0ecd70595a6e0ae0fe2a8ad1e6a37bfafe6df225
Парамонов Никита Сергеевич <paramonov_n@tochka.com>
2021-11-28 20:29:51 +0500

    Асинхронная задача

--- main.py
+++ main.py
@@ -1,40 +1,33 @@
 import time
-from urllib.request import urlopen
-from urllib.parse import unquote
-from bs4 import BeautifulSoup
-from tqdm import tqdm
 from urllib.request import Request, urlopen
-from urllib.parse import unquote
+import concurrent.futures
 
-url = 'https://ru.wikipedia.org/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:%D0%A1%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0'
 
-res = open('res.txt', 'w', encoding='utf8')
-
-for i in tqdm(range(100)):
-    html = urlopen(url).read().decode('utf8')
-    soup = BeautifulSoup(html, 'html.parser')
-    links = soup.find_all('a')
-
-    for l in links:
-        href = l.get('href')
-        if href and href.startswith('http') and 'wiki' not in href:
-            print(href, file=res)
-
-links = open('res.txt', encoding='utf8').read().split('\n')
-
-start_time = time.time()
-for url in links:
+def check_link(link):
     try:
         request = Request(
-            url,
+            link,
             headers={'User-Agent': 'Mozilla/5.0 (Windows NT 9.0; Win65; x64; rv:97.0) Gecko/20105107 Firefox/92.0'},
         )
-        resp = urlopen(request, timeout=5)
-        code = resp.code
-
+        response = urlopen(request, timeout=5)
+        code = response.code
         print(code)
-        resp.close()
+        response.close()
     except Exception as e:
-        print(url, e)
+        print("Exception occured. URL: ", link, "Exception:", e)
+
+
+links = open('res.txt', encoding='utf8').read().split('\n')
+worker_count = 5
+start_time = time.time()
+with concurrent.futures.ThreadPoolExecutor(max_workers=worker_count) as executor:
+    future_to_url = {executor.submit(check_link, url): url for url in links}
+    for future in concurrent.futures.as_completed(future_to_url):
+        url = future_to_url[future]
+        try:
+            data = future.result()
+        except Exception as exc:
+            print('%r generated an exception: %s' % (url, exc))
 end_time = time.time()
+
 print(end_time - start_time)
